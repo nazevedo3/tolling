857bb5b3084d00fd653962703b2d1318618334dd
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2023-07-26 21:10:42 -0700

    obu data and receiver

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nazevedo3/tolling
+
+go 1.19
+
+require github.com/gorilla/websocket v1.5.0
---
+++ types/types.go
@@ -0,0 +1,7 @@
+package types
+
+type OBUData struct {
+	OBUID int     `json:"obuid"`
+	Lat   float64 `json:"lat"`
+	Long  float64 `json:"long"`
+}
